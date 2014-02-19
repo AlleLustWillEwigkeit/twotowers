9a377dc596fc9a07f533c27495eab6bc37e451ac
Guba Dániel Olivér <pubdc@gmx.com>
2014-02-19 13:15:11 +0100

    Update Main.java
    Added origin of quote, ALwE. Testing purpose.

--- TwoTowers/src/allelustwillewigkeit/twotowers/Main.java
+++ TwoTowers/src/allelustwillewigkeit/twotowers/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Alle Lust will Ewigkeit!");
+		System.out.println("Alle Lust will Ewigkeit! (Friedrich Nietzsche)");
 		
 		
 	}
