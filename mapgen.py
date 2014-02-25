9c65fa01a723afb554835e9ad700367924eec2df
jentu <laszlobalogh94@gmail.com>
2014-02-25 18:48:20 +0100

    Rádiógombbal állítható, hogy mit akarsz settelni az egérhuzogattással miközben lenyomva tartod a bal egérgombot.

--- mapgen.py
+++ mapgen.py
@@ -1,7 +1,7 @@
 # -*- coding:utf8 -*-
 import Tkinter
 
-#HA LE AKAROD CSERÉLNI A PÁLYA MÉRETÉT, KÉRLEK ITT TEDD! 
+#HA LE AKAROD CSERÉLNI A PÁLYA MÉRETÉT, KÉRLEK ITT TEDD!
 MAGASSAG  = 25 # sorok száma
 SZELESSEG = 25 # oszlopok száma
 
@@ -55,29 +55,17 @@ def onCanvasLeftClick(event):
 def onDraggedWhileMouseButtonIsPressed(event):
 	global watDidIChangeLastX
 	global watDidIChangeLastY
-	hanyadikSor = int(event.y/FELBONTAS)
-	hanyadikOszlop = int(event.x/FELBONTAS)
-
-			
-	if(not ( (watDidIChangeLastX == hanyadikOszlop) and (watDidIChangeLastY == hanyadikSor) )):
-		debugLine.set("Miből: "+str(watDidIChangeLastX)+","+str(watDidIChangeLastY)+"\tMivé:"+str(hanyadikOszlop)+","+str(hanyadikSor))
-		#debugLine.set("Elvileg most átléptél!!")
-		watDidIChangeLastX = hanyadikOszlop
-		watDidIChangeLastY = hanyadikSor
-		aktualisElem = mezok[hanyadikSor][hanyadikOszlop]
-		volt = aktualisElem
-		if aktualisElem == EPITESI:
-			mezok[hanyadikSor][hanyadikOszlop] = UT
-			aktualisElem = UT
-		else:
-			mezok[hanyadikSor][hanyadikOszlop] = EPITESI
-			aktualisElem = EPITESI
-		lett = mezok[hanyadikSor][hanyadikOszlop]
-		debugLine.set(str("volt: ")+str(DEBUGBULLSHIT[volt])+str("\tlett:  ")+str(DEBUGBULLSHIT[lett])+"\tx="+str(hanyadikOszlop)+"\ty="+str(hanyadikSor) )
-		kirajzoloSzin = COLORZ[aktualisElem]
-		watDidIChangeLastX = hanyadikOszlop
-		watDidIChangeLastY = hanyadikSor
-		vaszon.create_rectangle(hanyadikOszlop*FELBONTAS+1, hanyadikSor*FELBONTAS+1, (hanyadikOszlop+1) * FELBONTAS+1, (hanyadikSor+1) * FELBONTAS+1, fill = kirajzoloSzin)
+	global radioGombAltalKivalasztott
+	if(   ((0 < event.x) and (0 < event.y) and (event.x < GRAFSZEL) and (event.y < GRAFMAG)) ): 
+		hanyadikSor    = int(event.y/FELBONTAS)
+		hanyadikOszlop = int(event.x/FELBONTAS)
+		if(not ( (watDidIChangeLastX == hanyadikOszlop) and (watDidIChangeLastY == hanyadikSor) )):
+			watDidIChangeLastX = hanyadikOszlop
+			watDidIChangeLastY = hanyadikSor
+			aktualis = radioGombAltalKivalasztott.get()
+			mezok[hanyadikSor][hanyadikOszlop] = radioGombAltalKivalasztott.get()
+			kirajzoloSzin = COLORZ[aktualis]
+			vaszon.create_rectangle(hanyadikOszlop*FELBONTAS+1, hanyadikSor*FELBONTAS+1, (hanyadikOszlop+1) * FELBONTAS+1, (hanyadikSor+1) * FELBONTAS+1, fill = kirajzoloSzin)
 
 
 #copy-paste is on my side with this one.
@@ -127,26 +115,37 @@ for y in range(MAGASSAG):
 #grafikai mainloop
 GRAFSZEL = SZELESSEG * FELBONTAS
 GRAFMAG = MAGASSAG * FELBONTAS
-
 ablakom = Tkinter.Tk () 
 vaszon = Tkinter.Canvas(ablakom, width = GRAFSZEL+1 , height = GRAFMAG+1)
-vaszon.grid(column = 0, row = 0, columnspan = 500, padx = 5, pady = 5 ) # senkivle sem lesz egy sorban
-vaszon.bind("<Button-1>", onCanvasLeftClick)
-vaszon.bind("<Button-3>", onCanvasRightClick)
-vaszon.bind("<B1-Motion>", onDraggedWhileMouseButtonIsPressed) 
 global debugLine
 debugLine = Tkinter.StringVar()
 debugger  = Tkinter.Label(ablakom,textvariable = debugLine)
-debugger.grid(column = 0, row = 1, columnspan = 500)
 kilepes = Tkinter.Button(ablakom, text = "EXIT", command = ablakom.destroy)
-kilepes.grid(column = 0, row = 3)
 mentes = Tkinter.Button(ablakom, text = "SAVE", command = butaKiir)
-mentes.grid(column = 0, row = 2)
 global HovaMentsen
 hovaMentsen = Tkinter.Entry(ablakom)
-hovaMentsen.grid(column = 1, row = 2)
 hovaMentsen.insert(0,"nothingSpecified.map")
 debugLine.set("This Is Just Debug")
+global radioGombAltalKivalasztott
+radioGombAltalKivalasztott = Tkinter.IntVar()
+
+
+#GRIDS
+vaszon.grid(column = 0, row = 0, columnspan = 500, padx = 5, pady = 5 ) # senkivle sem lesz egy sorban
+debugger.grid(column = 0, row = 1, columnspan = 500)
+kilepes.grid(column = 0, row = 3)
+mentes.grid(column = 0, row = 2)
+hovaMentsen.grid(column = 1, row = 2)
+Tkinter.Radiobutton(ablakom, text = DEBUGBULLSHIT[0], variable = radioGombAltalKivalasztott, value = 0).grid(column = 2, row = 2)
+Tkinter.Radiobutton(ablakom, text = DEBUGBULLSHIT[1], variable = radioGombAltalKivalasztott, value = 1).grid(column = 2, row = 3)
+#/GRIDS
+
+#BINDING
+vaszon.bind("<Button-1>", onCanvasLeftClick)
+vaszon.bind("<Button-3>", onCanvasRightClick)
+vaszon.bind("<B1-Motion>", onDraggedWhileMouseButtonIsPressed) 
+#/BINDING
+
 
 mindentKirajzol(mezok, vaszon, FELBONTAS , COLORZ)
 
