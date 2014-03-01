929607d664a04ee1da99661ed0cab21f9cd7def7
Hegyi Bálint <baprof.hegyi@gmail.com>
2014-03-01 19:58:07 +0100

    Main.java \n mentesítése

--- TwoTowers/src/allelustwillewigkeit/twotowers/Main.java
+++ TwoTowers/src/allelustwillewigkeit/twotowers/Main.java
@@ -3,10 +3,7 @@ package allelustwillewigkeit.twotowers;
 public class Main {
 
 	public static void main(String[] args) {
-
 		System.out.println("Alle Lust will Ewigkeit! (Friedrich Nietzsche)");
-		
-		
 	}
 
 }
