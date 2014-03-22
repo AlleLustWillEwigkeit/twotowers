dce75f23bcbc33f99fda6253648fead6735021fb
gudaol <pubdc@gmx.com>
2014-03-22 21:54:04 +0100

    fáradt vagyok én má kommenteli a commmito

--- TwoTowers/src/allelustwillewigkeit/twotowers/model/Ellensegek.java
+++ TwoTowers/src/allelustwillewigkeit/twotowers/model/Ellensegek.java
@@ -1,19 +1,22 @@
 package allelustwillewigkeit.twotowers.model;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 import allelustwillewigkeit.twotowers.skeleton.SkeletonController;
 
 public class Ellensegek {
-	public HashSet<Ellenseg> ellenseg = new HashSet<Ellenseg>();
+	public List<Ellenseg> ellenseg = new ArrayList<Ellenseg>();
 	public int osszLetszam = 0;
 	public JosagosSzaruman josagosSzaruman = null;
 	public Jatekmotor jatekMotor = null;
 	public Ellensegek ellensegek = null;
 	public Start start = null;
 
-	public Ellensegek() {
+	public Ellensegek(Jatekmotor jm) {
 		super();
+		jatekMotor = jm;
 	}
 
 	public void inditEllenseg(int mennyit) {
