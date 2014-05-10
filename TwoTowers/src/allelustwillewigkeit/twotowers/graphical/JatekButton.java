a911d2d0ca42b24eb0448746e41e9be95b9ce4d9
Cseh Dávid <david.cseh@rentit.hu>
2014-05-10 22:26:14 +0200

    Button

--- TwoTowers/src/allelustwillewigkeit/twotowers/graphical/JatekButton.java
+++ TwoTowers/src/allelustwillewigkeit/twotowers/graphical/JatekButton.java
@@ -7,41 +7,59 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.net.URL;
 
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 
 public class JatekButton extends JButton {
+	private enum MouseState{
+		NORMAL,HOVER 
+	}
+	
+	private enum ButtonState{
+		ACTIVE, INACTIVE, SELECTED 
+	}
+
 	
 	String name;
-	
+	MouseState mouseState;
+	ButtonState buttonState;
 	
 	private class ButtonMouseListener implements MouseListener {  //EZT OTTHON NE PRÓBÁLJÁTOK KI
 		@Override
 		public void mouseClicked(MouseEvent arg0) {
 			//TODO mezőre való kattintás
+			JatekButton gomb = (JatekButton)arg0.getSource();
+			gomb.mouseState = MouseState.NORMAL;
+			gomb.buttonState = ButtonState.SELECTED;
 		}
 
 		@Override
 		public void mouseEntered(MouseEvent arg0) {
 			JatekButton gomb = (JatekButton)arg0.getSource();
+			gomb.mouseState = MouseState.HOVER;
 		}
 
 		@Override
 		public void mouseExited(MouseEvent arg0) {
 			JatekButton gomb = (JatekButton)arg0.getSource();
+			gomb.mouseState = MouseState.NORMAL;
 		}
 
 		@Override
 		public void mousePressed(MouseEvent arg0) {
+			JatekButton gomb = (JatekButton)arg0.getSource();
 		}
 
 		@Override
 		public void mouseReleased(MouseEvent arg0) {
+			JatekButton gomb = (JatekButton)arg0.getSource();
 		}
 	}
 	
-	final static Color SELECTED = new Color(255, 255, 255, 100);
+	final static Color SELECTED = new Color(255, 0, 0, 100);
+	final static Color HOVER = new Color(255, 255, 255, 100);
 	final static Color TRANSPARENT = new Color(0, 0, 0, 0);
 	
 	ImageIcon activeIcon;
@@ -63,14 +81,39 @@ public class JatekButton extends JButton {
 		
 		this.setIcon(this.activeIcon);
 		this.setDisabledIcon(this.inActiveIcon);
+		
+		this.mouseState = MouseState.NORMAL;
+		this.buttonState = ButtonState.ACTIVE;
 	}
 	
 	@Override
 	protected void paintComponent(Graphics g) {
-		if(this.isEnabled())
-			g.drawImage(this.activeIcon.getImage(), 0, 0, null, null);
-		else
-			g.drawImage(this.inActiveIcon.getImage(), 0, 0, null, null);
+		super.paintComponent(g);
+		
+		Dimension pSize = this.getPreferredSize();
+		Dimension size = this.getSize();
+		
+		switch(this.buttonState){
+			case ACTIVE:
+				switch(this.mouseState){
+					case NORMAL:
+						g.setColor(TRANSPARENT);
+						break;
+					case HOVER:
+						g.setColor(HOVER);
+						break;
+				}
+				break;
+			case INACTIVE:
+				g.setColor(TRANSPARENT);
+				break;
+			case SELECTED:
+				g.setColor(SELECTED);
+				break;
+		}
+		
+		g.fillRect((size.width-pSize.width)/2, (size.height-pSize.height)/2, pSize.width, pSize.height);
+		
 	}
 	
 	
